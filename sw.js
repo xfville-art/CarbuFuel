37ad614797ae55cf8759021a6cf260a0e2b12e0a
xfville-art <xfville@gmail.com>
2026-03-25 09:13:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,29 @@
-const CACHE_NAME = 'carbufuel-v2';
+const CACHE_NAME = 'carbufuel-v3';
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  // Liste des logos locaux à mettre en cache
-  './assets/logos/total.png',
-  './assets/logos/leclerc.png',
-  './assets/logos/carrefour.png',
-  './assets/logos/intermarche.png',
-  './assets/logos/systemeu.png',
-  './assets/logos/auchan.png',
-  './assets/logos/esso.png',
-  './assets/logos/bp.png',
-  './assets/logos/shell.png',
-  './assets/logos/casino.png',
-  './assets/logos/netto.png',
-  './assets/logos/avia.png'
+  './icon-192.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Nettoyage de l'ancien cache lors de la mise à jour
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
     ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Stratégie : Réseau d'abord, sinon cache
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
